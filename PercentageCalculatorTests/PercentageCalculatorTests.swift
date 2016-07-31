a01386f4596c96b8dc88490fd8a54f92f77fd4d7
Romy Ilano <info@romyilano.com>
2016-07-31 16:23:57 -0700

    more swift 3 goodness

--- PercentageCalculatorTests/PercentageCalculatorTests.swift
+++ PercentageCalculatorTests/PercentageCalculatorTests.swift
@@ -11,14 +11,25 @@ import XCTest
 
 class PercentageCalculatorTests: XCTestCase {
     
+    var vc:ViewController!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        vc = storyboard.instantiateInitialViewController() as! ViewController
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+    
+    func testPercentageCalculator() {
+        
+        let p = vc.percentage(50, 50)
+        XCTAssert(p == 25)
+    
     }
     
     func testExample() {
